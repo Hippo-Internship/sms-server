c3ffad824fc0d70d462727f494c73567610b58ee
batsoyombo-dev <batsoyombo.kh@gmail.com>
2021-06-11 11:29:09 +0800

    s3 bug fix

--- core/settings/production.py
+++ core/settings/production.py
@@ -5,16 +5,15 @@ AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
 AWS_DEFAULT_ACL = None
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.amazonaws.com'
-_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.ap-east-1.amazonaws.com'
+AWS_S3_CUSTOM_DOMAIN = f'd3462nj2929uop.cloudfront.net'
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 # s3 static settings
 STATIC_LOCATION = 'static'
-STATIC_URL = f'https://{_DOMAIN}/{STATIC_LOCATION}/'
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
 STATICFILES_STORAGE = 'core.storages.StaticStorage'
 # s3 public media settings
 PUBLIC_MEDIA_LOCATION = 'media'
-MEDIA_URL = f'https://{_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
 DEFAULT_FILE_STORAGE = 'core.storages.PublicMediaStorage'
 
 STATICFILES_DIRS = (path.join(BASE_DIR, 'static'),)
