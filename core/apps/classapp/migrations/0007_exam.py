822e92987256802c840262661121317f2a096140
batsoyombo-dev <batsoyombo.kh@gmail.com>
2021-06-25 14:17:03 +0800

    test production commit

--- core/apps/classapp/migrations/0007_exam.py
+++ core/apps/classapp/migrations/0007_exam.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('classapp', '0006_alter_calendar_unique_together'),
+        ('classapp', '0005_alter_calendar_unique_together'),
     ]
 
     operations = [
