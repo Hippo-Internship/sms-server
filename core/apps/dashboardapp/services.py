7f7ea6733d41520c4b7180deaac252d38d40e185
batsoyombo-dev <batsoyombo.kh@gmail.com>
2021-06-26 18:48:58 +0800

    pending payment implementation

--- core/apps/dashboardapp/services.py
+++ core/apps/dashboardapp/services.py
@@ -3,37 +3,42 @@ from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
 # Django imports
 from django.contrib.auth import get_user_model
-from django.db.models.aggregates import Sum
+from django.db.models.aggregates import Min, Sum
 from django.db.models.query import QuerySet
 # Third party imports
 # Local imports
 from core.apps.studentapp import models as studentapp_models
+from core.apps.classapp import models as classapp_models
 
 User = get_user_model()
 
-def generate_total_income_data(user: User, filter_queries: dict={}, filter: int=3) -> dict:
+def generate_total_income_data(user: User, filter_queries: dict={}, filter: int=1) -> dict:
     queryset: QuerySet = studentapp_models.Payment.objects
+    class_queryset: QuerySet = classapp_models.Class.objects
     if user.groups.role_id == User.SUPER_ADMIN:
         payments = queryset.filter(**filter_queries)
-        all_time_start_date = user.date_joined
+        classes = class_queryset.filter(**filter_queries)
     elif user.groups.role_id == User.ADMIN:
         payments = queryset.filter(student__user__branch__school=user.school.id, **filter_queries)
-        all_time_start_date = user.school.created
+        classes = class_queryset.filter(branch__school=user.school.id, **filter_queries)
     else:
         payments = queryset.filter(student__user__branch=user.branch, **filter_queries)
-        all_time_start_date = user.branch.created
+        classes = class_queryset.filter(branch=user.branch, **filter_queries)
+    all_time_start_date = classes.aggregate(min_date=Min("start_date"))["min_date"]
+    all_time_start_date = datetime(all_time_start_date.year, all_time_start_date.month, all_time_start_date.day)
     income_by_filter: list = []
     income_dates: list = []
     total_income_by_filter: int = 0
     today_date = datetime.now()
     if filter == 1:
         for i in range(6, -1, -1):
-            temp_date = today_date - today_date - timedelta(days=i)
+            temp_date = today_date - timedelta(days=i)
             temp_income = payments.filter(date=temp_date).aggregate(income=Sum("paid"))
             real_income = temp_income["income"] if temp_income["income"] is not None else 0
             income_by_filter.append(real_income)
             total_income_by_filter += real_income
             income_dates.append(temp_date.strftime("%Y-%m-%d"))
+        temp_total_lesson_price = classes.filter(start_date__range=[ today_date - timedelta(days=7), today_date ]).aggregate(total=Sum("lesson__price"))
     elif filter == 2:
         for i in range(12, 0, -1):
             temp_date = today_date - relativedelta(months=i - 1) 
@@ -46,6 +51,7 @@ def generate_total_income_data(user: User, filter_queries: dict={}, filter: int=
             income_by_filter.append(real_income)
             total_income_by_filter += real_income
             income_dates.append(temp_date.strftime("%Y-%m-%d"))
+        temp_total_lesson_price = classes.filter(start_date__range=[ today_date - relativedelta(months=12) , today_date ]).aggregate(total=Sum("lesson__price"))
     elif filter == 3:
         delta_days: int = (today_date.date() - all_time_start_date.date()).days
         _delta_days = int(delta_days / 12)
@@ -61,12 +67,19 @@ def generate_total_income_data(user: User, filter_queries: dict={}, filter: int=
             income_by_filter.append(real_income)
             total_income_by_filter += real_income
             income_dates.append(temp_date.strftime("%Y-%m-%d"))
+        temp_total_lesson_price = classes.filter(start_date__range=[ today_date - timedelta(days=delta_days), today_date ]).aggregate(total=Sum("lesson__price"))
+    temp_pending = temp_total_lesson_price["total"] if temp_total_lesson_price["total"] is not None else 0
+    real_pending = temp_pending - total_income_by_filter
     generated_data: dict = {
         "income": {
             "total": total_income_by_filter,
             "data": income_by_filter,
             "dates": income_dates
         },
+        "pending": real_pending
     }
-    print(generated_data)
     return generated_data
+
+def generate_students_data(user, filter_queries: dict={}, filter: int=3) -> dict:
+
+    return 12
--- core/apps/dashboardapp/views.py
+++ core/apps/dashboardapp/views.py
@@ -6,7 +6,8 @@ from rest_framework import \
         decorators as rest_decorators
 # Local imports
 from core import \
-        responses as core_responses
+        responses as core_responses, \
+        utils as core_utils
 from . import services as local_services
 
 # Create your views here.
@@ -14,5 +15,38 @@ class DashboardViewset(viewsets.GenericViewSet):
 
     @rest_decorators.action(detail=False, methods=[ "GET" ], url_path="payment")
     def list_payment_data(self, request):
-        income_data = local_services.generate_total_income_data(request.user)
-        return core_responses.request_success_with_data()
+        request_user = request.user
+        query_params = core_utils.normalize_data(
+            {
+                "branch": "int",
+                "school": "int",
+                "filter": "int"
+            },
+            dict(request.query_params)
+        )
+        filter_model = {
+            "branch": "student__user__branch",
+            "school": "student__user__branch__school",
+        }
+        filter_queries = core_utils.build_filter_query(filter_model, query_params, user=request_user)
+        income_data = local_services.generate_total_income_data(request_user, filter_queries=filter_queries, filter=query_params.get("filter", 1))
+        return core_responses.request_success_with_data(income_data)
+        
+    @rest_decorators.action(detail=False, methods=[ "GET" ], url_path="student")
+    def list_student_data(self, request):
+        request_user = request.user
+        query_params = core_utils.normalize_data(
+            {
+                "branch": "int",
+                "school": "int",
+                "filter": "int"
+            },
+            dict(request.query_params)
+        )
+        filter_model = {
+            "branch": "student__user__branch",
+            "school": "student__user__branch__school",
+        }
+        filter_queries = core_utils.build_filter_query(filter_model, query_params, user=request_user)
+        student_data = local_services.generate_students_data(request_user, filter_queries=filter_queries, filter=query_params.get("filter", 1))
+        return core_responses.request_success()
