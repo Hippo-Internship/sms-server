293e553645ee842c50f2d7074c081284b9c13d53
batsoyombo-dev <batsoyombo.kh@gmail.com>
2021-07-09 12:46:12 +0800

    code change

--- core/apps/classapp/views.py
+++ core/apps/classapp/views.py
@@ -314,21 +314,6 @@ class ClassViewSet(viewsets.GenericViewSet):
         students = self.get_serializer_class()(p_student, many=True)
         return self.get_paginated_response(students.data)
 
-    # @list_students.mapping.delete
-    # @core_decorators.has_key("student")
-    # @core_decorators.object_exists(model=local_models.Class, detail="Class")
-    # def cancel_student(self, request, _class=None):
-    #     student = request.data["student"]
-    #     if type(student).__name__ != "list":
-    #         return core_responses.request_denied()
-    #     student = _class.students.filter(id=student)
-    #     if student.exists():
-    #         return core_responses.request_denied()
-    #     student = student[0]
-    #     student.canceled = False
-    #     student.save()
-    #     return core_responses.request_success()
-
     @list_students.mapping.post
     @core_decorators.object_exists(model=local_models.Class, detail="Class")
     def create_student(self, request, _class=None):
